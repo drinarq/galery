0ac7ca8a5108dd10047359fb531915801587d000
drinar <drinarq@gmail.com>
2020-10-11 22:50:52 +0300

    init

---
+++ src/view/registration.tsx
@@ -0,0 +1,14 @@
+import React, { useCallback, useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import '../styles/registr.css';
+
+function Registration() {
+    return(
+        <div className="container">
+            
+        </div>
+    )
+}
+
+export default Registration;
